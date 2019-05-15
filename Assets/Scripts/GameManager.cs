eb40b4ebe47565930a6b8ccffc3ed3558d165e69
balajiraghuram92 <balajiraghuram92@gmail.com>
2019-05-15 18:06:50 +0530

    Commit 3 : JobComponentSystem implementation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+     private static GameManager _instance = null;
+
+     public static GameManager Instance {
+         get
+         {
+               return _instance;
+         }
+     }
+
+    //common variables
+    public float MoveSpeed = 3.0f;
+    public float TopBounds = 3.0f;
+    public float BottomBounds = -48.0f;
+    public float LeftBounds = -20.5f;
+    public float RightBounds = 20.5f; 
+
+    public float count = 0;
+
+     
+     void Awake()
+     {
+         if(_instance != null && _instance != this)
+            Destroy(_instance);
+        else if(_instance == null)
+        {
+            _instance = this;
+        }
+
+     }
+}
